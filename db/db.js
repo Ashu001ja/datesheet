cbe5ee702d96d121e2c06e895296ad8da04917c8
Ashwani452 <ashwanijangra45@gmail.com>
2024-09-08 07:48:53 +0530

    final changes

--- db/db.js
+++ db/db.js
@@ -4,7 +4,7 @@ URL=process.env.MONGODB
 const connectDB=()=>{
     mongoose.connect(URL).then(()=>{
         console.log('MongoDB Connected...');
-    }).catch((err)=>console.log(err));
+    })
 };
 
 module.exports = connectDB;
